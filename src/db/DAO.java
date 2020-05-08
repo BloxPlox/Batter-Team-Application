d07de014d2bf0dcbc86e65ac5ccac5d1a692d10c
James <James@James-PC.home>
2020-05-07 23:58:02 -0400

    Removed interface.

---
+++ src/db/DAO.java
@@ -0,0 +1,19 @@
+/*
+Batter Application Final Project
+Author: Nathan Minnick
+Date: 4/25/2020
+File: DAO.java
+*/
+
+package db;
+
+import java.util.List;
+
+//creates the DAO interface
+public interface DAO<T> {
+    //T get(int batID);
+    //List<T> getAll();
+    //boolean add(T t);
+    //boolean update(T t);
+    //boolean delete(T t);
+}
